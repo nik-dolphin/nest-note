7ac3d238a5fa9c12fb39d266a98a9874000f7eb0
“divyang-dolphin” <nikunj.dolphinwebsolution@gmail.com>
2022-11-25 16:22:37 +0530

    feat: hide mongoDB cluster rootPath

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ProductsModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://nikunj:RhfUiZul82ZMd1OR@cluster0.bl2o5uf.mongodb.net/nest-note',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGOOSE_MODULE_ROOT),
   ],
   controllers: [AppController],
   providers: [AppService],
